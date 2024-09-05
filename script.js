8a6b554bd8f29eab14f7f88558b3f8e765e73e99
FlyingTechGuy <146762239+FlyingTechGuy@users.noreply.github.com>
2024-09-05 10:26:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
         document.querySelector(".topSubTitle").classList.add("show");
         document.querySelector(".blackopacity").classList.add("show");
         document.querySelector(".titleBox").classList.add("show");
-    }, 3000);
+    }, 2000);
 })
 
 showSlides(1);
@@ -31,4 +31,156 @@ function showSlides(n) {
         dot1.classList.remove("active");
         dot2.classList.add("active");
     }
+}
+
+// Get the modal
+const modal = document.getElementById("myModal");
+			
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img1 = document.getElementById("myImg1");
+const img2 = document.getElementById("myImg2");
+const img3 = document.getElementById("myImg3");
+const img4 = document.getElementById("myImg4");
+const img5 = document.getElementById("myImg5");
+const img6 = document.getElementById("myImg6");
+const img7 = document.getElementById("myImg7");
+const img8 = document.getElementById("myImg8");
+const img9 = document.getElementById("myImg9");
+const img10 = document.getElementById("myImg10");
+const img11 = document.getElementById("myImg11");
+const img12 = document.getElementById("myImg12");
+const img13 = document.getElementById("myImg13");
+const img14 = document.getElementById("myImg14");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+img1.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img3.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img4.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img5.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img6.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img7.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img8.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img9.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img10.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img11.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img12.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img13.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img14.onclick = function(){
+  modal.style.display = "flex";
+  setTimeout(function() {
+    modal.classList.add('show');
+  }, 1);
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+const span = document.getElementById("close");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.classList.remove('show');
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 200);
+}
+
+// When the user clicks on the bacground, close the modal
+function closeModal() {
+  modal.classList.remove('show');
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 200);
 }
